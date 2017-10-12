279f24848ac28ddb496d5af89a5fd67cab98b468
simon <who8023me@gmail.com>
2017-10-12 16:14:57 +0800

    add some specify commit to test javadoc

--- Property.java
+++ Property.java
@@ -1,5 +1,17 @@
+//:Property.java
 import java.util.*;
+/** The first Thinking in java example program.
+*Lists system information on current machine.
+*@author simon.jia
+*@author http://www.simon.jia.com
+*@version 1.0
+*/
 public class Property {
+	/**Sole entry point to class & application
+	*@param args array of string arguments
+	*@return No return value
+	*@exception exceptions No exception thrown
+	*/
 	public static void main (String[] args) {
 	System.out.println(new Date());
 	Properties p = System.getProperties();
@@ -11,5 +23,5 @@ public class Property {
 			   +"Free Memory = "
 			   +rt.freeMemory());
   }
-}
+}///:
 
