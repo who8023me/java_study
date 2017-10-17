a2ba369ebc7288275f51e4657739b9f077d33cd1
simon <who8023me@gmail.com>
2017-10-17 16:31:29 +0800

    java io stream example programme

---
+++ OutFile.java
@@ -0,0 +1,16 @@
+//: OutFile.java
+// Shorthand class for opening an output file
+// for data storage.
+import java.io.*;
+public class OutFile extends DataOutputStream 
+{
+	public OutFile(String filename)
+	throws IOException 
+	{
+		super(new BufferedOutputStream(new FileOutputStream(filename)));
+	}
+	public OutFile(File file)throws IOException 
+	{
+		this(file.getPath());
+	}
+} ///:~
---
+++ PrintFile.java
@@ -0,0 +1,13 @@
+//: PrintFile.java
+// Shorthand class for opening an output file
+// for human-readable output.
+import java.io.*;
+public class PrintFile extends PrintStream 
+{
+	public PrintFile(String filename) throws IOException 
+	{
+		super(new BufferedOutputStream(new FileOutputStream(filename)));
+	}
+		public PrintFile(File file)	throws IOException {this(file.getPath());
+	}
+} ///:~
